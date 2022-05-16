5559428e01d59c0b9d8e5b8909d92d9273b723dc
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 19:58:53 -0400

    create snake body

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
 # TODO: Create a snake body
+segment_1 = Turtle(shape="square")
+segment_1.color("white")
+
+segment_2 = Turtle(shape="square")
+segment_2.color("white")
+segment_2.goto(-20,0)
+
+segment_3 = Turtle(shape="square")
+segment_3.color("white")
+segment_3.goto(-40,0)
+
 # TODO: How to move the snake
 # TODO: Control the snake
-
-
 # TODO: Detect collision with food
 # TODO: Create a scoreboard
 # TODO: Detect collision with wall
 # TODO: Detect collision with tail
 
+screen.exitonclick()
