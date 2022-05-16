cb8b43f8ae8bf5ed154461eb68f18fbcdc683557
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 19:49:03 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO: Create a snake body
+# TODO: How to move the snake
+# TODO: Control the snake
+
+
+# TODO: Detect collision with food
+# TODO: Create a scoreboard
+# TODO: Detect collision with wall
+# TODO: Detect collision with tail
+
