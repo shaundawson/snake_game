c6b284a8858593992b838e60a42f23fdf20e1519
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-16 20:08:44 -0400

    create snake body for loop

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 # TODO: Create a snake body
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
+# segment_1 = Turtle(shape="square")
+# segment_1.color("white")
 
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(-20,0)
+# segment_2 = Turtle(shape="square")
+# segment_2.color("white")
+# segment_2.goto(-20,0)
 
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(-40,0)
+# segment_3 = Turtle(shape="square")
+# segment_3.color("white")
+# segment_3.goto(-40,0)
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+    
+
 
 # TODO: How to move the snake
 # TODO: Control the snake
