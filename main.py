3e32d27f77ec8b73a2b1bbe49ef5dc742492c409
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-17 18:56:21 -0400

    animate snake segments to move forward

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 # TODO: Create a snake body
 # segment_1 = Turtle(shape="square")
@@ -18,14 +20,33 @@ screen.title("Snake Game")
 # segment_3.goto(-40,0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
+
+game_is_on = True
+# TODO: Animate the snake segments to move forward
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments)-1,0,-1):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num -1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+
+
+
+
+
+
     
-
-
-# TODO: How to move the snake
 # TODO: Control the snake
 # TODO: Detect collision with food
 # TODO: Create a scoreboard
