07378d76c86de4036e28934f9ec930f3f850f5ab
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-19 16:30:38 -0400

    detect collision with tail

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
         
         # TODO: Detect collision with wall
--- snake.py
+++ snake.py
@@ -29,11 +29,20 @@ class Snake:
     
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+           
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    
+    
+    def extend(self):
+        #add a new segment to the snake
+        self.add_segment(self.segments[-1].position())
 
 
     # TODO: Animate the snake segments to move forward
