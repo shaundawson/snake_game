7e82014dc374a38aa2660ec15fcba92fb921722b
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-19 16:42:24 -0400

    update main

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
@@ -22,12 +23,15 @@ screen.onkey(snake.down,"Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     
+
     snake.move()
+
 
     #Detect collision with food
     if snake.head.distance(food) < 15:
@@ -35,15 +39,18 @@ while game_is_on:
         snake.extend()
         scoreboard.increase_score()
         
-        # TODO: Detect collision with wall
+
+    # TODO: Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
-    
 
 
-
-
-# TODO: Detect collision with tail
+    # TODO: Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+            
 
 screen.exitonclick()
