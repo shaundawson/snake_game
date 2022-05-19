bb5261dc8958342f1bb1a398d02f5861b64416a3
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-19 16:05:56 -0400

    increase score

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,11 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+
+# TODO: Create a scoreboard
 class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
@@ -7,6 +13,17 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0,250)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
 
